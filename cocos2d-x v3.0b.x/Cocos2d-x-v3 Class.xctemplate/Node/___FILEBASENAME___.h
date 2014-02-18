9dfee5a55a855b2edc4b36ccbb0bc0a115a15df6
杏ｚ <an@shingo-no-MacBook-Pro-3.local>
2014-02-18 14:42:41 +0900

    cocos2dx v3.0b2 用テンプレート追加

---
+++ cocos2d-x v3.0b.x/Cocos2d-x-v3 Class.xctemplate/Node/___FILEBASENAME___.h
@@ -0,0 +1,29 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "cocos2d.h"
+
+class ___FILEBASENAME___ : public cocos2d::___VARIABLE_cocos2DXv3Subclass:identifier___
+{
+    
+public:
+    ___FILEBASENAME___();
+    virtual ~___FILEBASENAME___();
+    virtual bool init();
+    CREATE_FUNC(___FILEBASENAME___);
+    
+protected:
+    
+private:
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
